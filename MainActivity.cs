6e425aa620da4cf2ffdcb55787d878dae6838ba0
Admin <>
2014-06-26 21:00:38 +0530

    dd

--- MainActivity.cs
+++ MainActivity.cs
@@ -23,7 +23,9 @@ namespace eterw
 
 			// Get our button from the layout resource,
 			// and attach an event to it
+			//Test
 			Button button = FindViewById<Button> (Resource.Id.myButton);
+
 			
 			button.Click += delegate {
 				button.Text = string.Format ("{0} clicks!", count++);
